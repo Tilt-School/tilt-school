25d30808048bf54bf7249c0b6bf78413379ad18f
Andrii Herchykov <andriiwp3@gmail.com>
2022-06-26 10:27:25 +0100

    feature: rename tilt-school to tilt-school-landing

--- apps/tilt-school-landing-e2e/src/integration/app.spec.ts
+++ apps/tilt-school-landing-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('tilt-school', () => {
+describe('tilt-school-landing', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('tilt-school', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome tilt-school');
+    getGreeting().contains('Welcome tilt-school-landing');
   });
 });
